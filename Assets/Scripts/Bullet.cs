1d201fe9b2bbdc162ebdc5ece1dd9a407898697d
Andy Yiu <andyiu5402@gmail.com>
2024-03-29 00:28:08 +0800

    shooting

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public Vector3 currentlocation;
-    public Vector3 previouslocation;
-    public Vector3 direction;
+    public Vector2 currentlocation;
+    public Vector2 previouslocation;
+    public Vector2 direction;
     public Quaternion rotation;
-    public Vector3 eulerAngles;
     public Quaternion rotation2;
+
+    public GameObject portalPrefab1;
+    public GameObject portalPrefab2;
+
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         currentlocation = transform.position;
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
+
     // Update is called once per frame
+
     void FixedUpdate()
     {
+        // bullet direction
         previouslocation = currentlocation;
         currentlocation = transform.position;
         direction = currentlocation - previouslocation;
@@ -28,5 +36,30 @@ public class Bullet : MonoBehaviour
         rotation2.z = rotation.z;
         rotation2.w = rotation.w;
         transform.rotation = rotation2;
+        
     }
+
+    public void Launch (Vector3 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        // portal
+        if(collision.tag == "wall")
+        {
+            Destroy(gameObject);
+            if (rotation2.z < 0) { 
+                GameObject Object = Instantiate(portalPrefab1, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+            }
+            else if (rotation2.z > 0)
+            {
+                GameObject Object = Instantiate(portalPrefab2, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+            }
+            
+        }
+        
+    }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,14 +23,17 @@ public class PlayerController : MonoBehaviour
     GameObject portal1;
     GameObject portal2;
 
+    public GameObject projectilePrefab;
+    Vector2 rightDirection = new Vector2(1, 0);
+    Vector2 leftDirection = new Vector2(-1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
-        portal1 = GameObject.FindWithTag("portal1");
-        portal2 = GameObject.FindWithTag("portal2");
+
 
         animator.SetFloat("LookX", 1f);
         invincibleTimer = timeInvincible;
@@ -39,6 +42,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (horizontal != 0f)
         {
             if (horizontal > 0)
@@ -64,10 +68,19 @@ public class PlayerController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        // shooting
+
+        if (Input.GetMouseButtonDown(0)) {
+            Launch();
+        }
+        //Debug.Log(horizontal);
     }
 
     private void FixedUpdate()
     {
+        portal1 = GameObject.FindWithTag("portal1");
+        portal2 = GameObject.FindWithTag("portal2");
         horizontal = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
     }
@@ -112,5 +125,24 @@ public class PlayerController : MonoBehaviour
         HP = Mathf.Clamp(HP, 0, MaxHP);
 
         Debug.Log("Current HP: " + HP + "/" + MaxHP);
+    }
+    void Launch() {
+        GameObject projectileObject = Instantiate(projectilePrefab, rb.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Bullet projectile = projectileObject.GetComponent<Bullet>();
+        if (horizontal == 0)
+        {
+            projectile.Launch(rightDirection, 500);
+        }
+        else if (horizontal > 0)
+        {
+            projectile.Launch(rightDirection, 500);
+        }
+        else if (horizontal < 0)
+        {
+            projectile.Launch(leftDirection, 500);
+        }
+
+        //animator.SetTrigger("Launch");
     }
 }
